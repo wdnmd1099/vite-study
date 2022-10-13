a05e67760d96e9f344d00d05cc725e9a95a94b2a
Kasan <879611700@qq.com>
2022-10-13 21:54:03 +0800

    refactor: 重构 main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import { createApp } from 'vue'
 import { App } from './App'
-import { Foo } from './views/Foo'
-import {createRouter,createWebHashHistory} from 'vue-router'
-import { Bar } from './views/Bar'
+import {createRouter} from 'vue-router'
+import { routes } from './config/routes'
+import { history } from './shared/history'
 
-const routes = [
-    { path: '/', component:Foo},
-    { path: '/about',component:Bar},
-]
+
+
 
 const router = createRouter({
-    history:createWebHashHistory(),
+    history,
     routes,
 })
 

721b656ad01e840977081a3492667abc4e19257d
Kasan <879611700@qq.com>
2022-10-13 22:32:52 +0800

    feature: 创建欢迎页面的页面

---
+++ src/components/welcome/first.tsx
@@ -0,0 +1,6 @@
+import { defineComponent, ref } from "vue";
+export const First = defineComponent({
+setup(props,context){
+return ()=>(<div>First</div>)
+}
+})
---
+++ src/components/welcome/second.tsx
@@ -0,0 +1,6 @@
+import { defineComponent, ref } from "vue";
+export const Second = defineComponent({
+setup(props,context){
+return ()=>(<div>2</div>)
+}
+})

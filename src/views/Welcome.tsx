bf7a80cef57865f9366d65847398ad76d27d1282
Kasan <879611700@qq.com>
2022-10-15 15:11:17 +0800

    updata-1

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -2,10 +2,12 @@ import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
 
 export const Welcome = defineComponent({
-setup(props,context){
-return ()=>(
-<div>
-<RouterView/>
-</div>)
-}
+    setup(props, context) {
+        return () => 
+        <>
+        <header>logo</header>
+        <main><RouterView /></main>
+        <footer>buttons</footer>
+        </>
+    }
 })
